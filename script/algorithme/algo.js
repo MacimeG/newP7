3bfb858b40ce398e8ba022b46b2f7c11543dc08c
MacimeG <maxime.glaneux@gmail.com>
2022-11-04 11:41:06 +0100

    finition css

--- script/algorithme/algo.js
+++ script/algorithme/algo.js
@@ -7,9 +7,9 @@
 export default function searchRecipe(lettre, recipesFiltered){
     // alors ici, je met en place une constante, qui contient mes conditions(qui filtre le resultat de la recherche et va chercher dans le tableau les element correspondant), et retourne les recettes correspondante a la valeur de l'input. je met des || (ou) pour tout comparer directement.
 
-    const recipesFiltre = recipesFiltered.filter((recipe) => recipe.name.toLowerCase().includes(lettre) || recipe.description.toLowerCase().includes(lettre) ||
+    const recipesFiltre = recipesFiltered.filter((recipe) => recipe.name.toLowerCase().includes(lettre.toLowerCase()) || recipe.description.toLowerCase().includes(lettre.toLowerCase()) ||
     recipe.ingredients.some((ingObj) => {
-        ingObj.ingredient.toLowerCase().includes(lettre)
+        ingObj.ingredient.toLowerCase().includes(lettre.toLowerCase())
     }) )
    
     
